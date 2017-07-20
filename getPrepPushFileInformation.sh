2e6c7aa85616f72c33b997c88a2e151756d21d4d
Aleksandra Malesa <a_malesa@hotmail.co.uk>
2017-07-20 12:22:05 +0100

    get a list of all the commited but not pushed files

---
+++ getPrepPushFileInformation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+UN_PUSHED_FILES_STRING=$(git log origin/$BRANCH..$BRANCH --name-only --pretty="format:" | sort | uniq
+)
+
+echo '1 --------------'
+echo $BRANCH
+echo '2 --------------'
+echo $UN_PUSHED_FILES_STRING
+
+if [[ $UN_PUSHED_FILES_STRING == *".js"* ]] 
+then
+echo "run karma test"
+fi
+if [[ $UN_PUSHED_FILES_STRING != *".scss"* ]] 
+then
+echo "build scss"
+fi
